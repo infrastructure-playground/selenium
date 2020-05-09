ad59d3559f36db4e9ac23f3f62cf2f5ba3bbf0f5
dallas_city <dallas@unnotechmanila.com>
2020-05-10 00:12:05 +0800

    Code Optimization

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ from selenium.webdriver.firefox.options import Options as Firefox_Options
 
 class UITesting(unittest.TestCase):
 
-    redirection_delay = 3
-    registered_user = 'UI_test'
+    redirection_delay = 2
 
     def setUp(self):
-        chrome_options = Chrome_Options()
-        chrome_options.add_argument("--headless")
-        self.browser = webdriver.Remote(command_executor="http://selenium:4444/wd/hub", desired_capabilities=chrome_options.to_capabilities())
+        self.options.add_argument("--headless")
+        self.browser = webdriver.Remote(
+            command_executor=f"http://{self.selenium_server}:4444/wd/hub",
+            desired_capabilities=self.options.to_capabilities())
         self.addCleanup(self.browser.quit)
 
     def testLogin(self):
@@ -38,7 +38,7 @@ class UITesting(unittest.TestCase):
         self.browser.get('http://192.168.1.115:3000/register')
         # Enter non-existing username
         username = self.browser.find_element_by_id('username')
-        username.send_keys(self.registered_user)
+        username.send_keys(f'UI_{self.platform}_test')
         # Enter password
         password = self.browser.find_element_by_id('password')
         password.send_keys('pass1234')
@@ -53,29 +53,23 @@ class UITesting(unittest.TestCase):
         inventory_list = self.browser.find_element_by_id('book-inventory')
 
     def tearDown(self):
-        requests.get('http://192.168.1.115:8000/delete-test-registered-user/')
+        requests.get(f'http://192.168.1.115:8000/'
+                     f'delete-test-registered-user/'
+                     f'?platform={self.platform}')
 
 
 class GoogleChromeCompatibility(UITesting):
 
-    registered_user = 'UI_Chrome_test'
-
-    def setUp(self):
-        chrome_options = Chrome_Options()
-        chrome_options.add_argument("--headless")
-        self.browser = webdriver.Remote(command_executor="http://selenium:4444/wd/hub", desired_capabilities=chrome_options.to_capabilities())
-        self.addCleanup(self.browser.quit)
+    selenium_server = "selenium"
+    options = Chrome_Options()
+    platform = 'Chrome'
 
 
 class FirefoxCompatibility(UITesting):
 
-    registered_user = 'UI_Firefox_test'
-
-    def setUp(self):
-        firefox_options = Firefox_Options()
-        firefox_options.add_argument("--headless")
-        self.browser = webdriver.Remote(command_executor="http://selenium-firefox:4444/wd/hub", desired_capabilities=firefox_options.to_capabilities())
-        self.addCleanup(self.browser.quit)
+    selenium_server = "selenium-firefox"
+    options = Firefox_Options()
+    platform = 'Firefox'
 
 
 if __name__ == '__main__':
